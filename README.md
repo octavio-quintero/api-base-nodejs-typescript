4fc95d8a6543a756df49ff77ec81e822418fcdad
Octavio Quintero <octavio.quintero@outlook.com>
2020-04-27 11:14:40 -0600

    Add initial project

---
+++ README.md
@@ -0,0 +1,8 @@
+# Project Base for Node.js applications written in TypeScript
+TODO: Give a short introduction of your project. Let this section explain the objectives or the motivation behind this project. 
+
+# Development
+npm run dev
+
+# Running tests
+npm test
---
+++ src/App.ts
@@ -0,0 +1,32 @@
+import * as express from "express";
+import * as bodyParser from "body-parser";
+
+class App {
+
+    public app: express.Application;
+
+    constructor() {
+        this.app = express();
+        this.config();        
+    }
+
+    private config(): void{
+        // support application/json type post data
+        this.app.use(bodyParser.json());
+        //support application/x-www-form-urlencoded post data
+        this.app.use(bodyParser.urlencoded({ extended: false }));
+
+        let router = express.Router();
+
+        // placeholder route handler
+        router.get('/', (req, res, next) => {
+            res.json({
+                message: 'Hello World!'
+            });
+        });
+        this.app.use('/', router);
+    }
+
+}
+
+export default new App().app;
